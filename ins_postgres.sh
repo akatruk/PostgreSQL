b0dcbaee07613a355dc9be46bd88dad12d837701
akatruk <a.katruk@live.com>
2019-01-22 14:57:13 +0300

    install postgres

--- ins_postgres.sh
+++ ins_postgres.sh
@@ -8,23 +8,21 @@ apt install postgresql-client-common gcc libreadline-dev bzip2 zlib1g-dev make
 #unpacked file
 cd /home/akatruk/Downloads/
 tar xvjf postgrespro-standard-11.1.1.tar.bz2
-rm -rf /home/akatruk/Downloads/postgrespro-standard-11.1.1.tar.bz2
 
 #create user postgres
 useradd -m -d /home/postgres postgres
 
 #create directory for distributive
 rm -rf /u01/psql/
-mkdir -r /u01/psql/
-chown postgres /u01/psql/
-chmod 777 /u01/psql/
+mkdir -p /u01/psql/
+chown postgres -R /u01/psql/
+chmod -R 777 /u01/psql/
 
 #prepare instalation postgres
 cd /home/akatruk/Downloads/postgrespro-standard-11.1.1/
 ./configure -prefix /u01/pgsql
 
 #install postgres
-cd /u01/psql/
 make install
 
 #remove installation distributive
