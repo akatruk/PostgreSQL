0215de48c89ec62d7a5522d7145f7749bd15127a
akatruk <a.katruk@live.com>
2019-09-09 13:47:35 +0300

    09.09

--- gp_analize_db.sql
+++ gp_analize_db.sql
@@ -14,3 +14,14 @@ SELECT
     ORDER BY 
       ratio DESC;
 
+-- Table that is using the most disk space in your database
+SELECT 
+       relname AS "table_name", 
+       pg_size_pretty(pg_table_size(C.oid)) AS "table_size" 
+FROM 
+       pg_class C 
+LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) 
+WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND nspname !~ '^pg_toast' AND relkind IN ('r') 
+ORDER BY pg_table_size(C.oid) 
+DESC LIMIT 1;
+
