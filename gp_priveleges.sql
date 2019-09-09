0725cc2427e71e5bfe573c366cbdfc4d3dba9816
akatruk <a.katruk@live.com>
2019-09-09 13:47:25 +0300

    09.09

--- gp_priveleges.sql
+++ gp_priveleges.sql
@@ -1,25 +1,32 @@
-## How to grant privilege
-create role team_ga nologin;
+-- Grant privilege
+create role team_rms nologin;
 create role "60076294" login;         
 grant team_ga to "60076294"; 
-grant select on td_idwh1_bv_prod_009_pbsdbs_ods to team_rms;
+grant select on td_idwh1_bv_prod_009_pbsdbs_raw to team_rms;
 grant select on rms_p009qtzb_rms_ods.item_master to team_ga;
-REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA rms_p009qtzb_rms_ods FROM team_rms;
+-- Grant privilege on schema
+GRANT USAGE ON SCHEMA rms_p009qtzb_rms_raw TO team_rms;
 
 
-## Granted privilege
-SELECT table_catalog, table_schema, table_name, privilege_type
-FROM   information_schema.table_privileges 
-WHERE  grantee = 'team_ga';
+-- Drop user
+DROP USER team_rms;
+REVOKE ALL PRIVILEGES ON SCHEMA rms_p009qtzb_rms_raw FROM team_rms;
 
-## Find objects in database
+-- Granted privilege
+SELECT table_catalog, table_schema, table_name, privilege_type FROM information_schema.table_privileges WHERE  grantee = 'team_rms';
 
-## Find specific schema
-SELECT nspname FROM pg_catalog.pg_namespace order by nspname where nspname like '%rms%';
+----------------------------------------
+-- Find objects in database --
+-- Find specific schema
+SELECT nspname 
+FROM pg_catalog.pg_namespace 
+WHERE nspname like '%rms%';
 
-## List all tables in specific schema
+-- List all tables in specific schema
 SELECT table_name FROM information_schema.tables WHERE table_schema='rms_p009qtzb_rms_raw';
 
 SELECT table_name FROM information_schema.tables 
 WHERE table_schema='td_idwh1_bv_prod_009_pbsdbs_ods'
 and table_name like 'tf009_vte_ligticcai';
+
+GRANT USAGE ON SCHEMA rms_p009qtzb_rms_raw to team_rms;
