0626bba09b0197ca1a0b19bd9451741fefdbd6d8
akatruk <a.katruk@live.com>
2019-09-10 19:08:04 +0300

    10.09

--- gp_priveleges.sql
+++ gp_priveleges.sql
@@ -1,12 +1,12 @@
 -- Grant privilege
 create role team_rms nologin;
-create role "60076294" login;         
-grant team_ga to "60076294"; 
+create role "60065275" login;         
+grant team_rms to "akatruk"; 
+grant ldap_users to "akatruk"; 
 grant select on td_idwh1_bv_prod_009_pbsdbs_raw to team_rms;
 grant select on rms_p009qtzb_rms_ods.item_master to team_ga;
 -- Grant privilege on schema
-GRANT USAGE ON SCHEMA rms_p009qtzb_rms_raw TO team_rms;
-
+GRANT USAGE ON SCHEMA metadata TO team_rms;
 
 -- Drop user
 DROP USER team_rms;
@@ -20,13 +20,55 @@ SELECT table_catalog, table_schema, table_name, privilege_type FROM information_
 -- Find specific schema
 SELECT nspname 
 FROM pg_catalog.pg_namespace 
-WHERE nspname like '%rms%';
+WHERE nspname like '%metadata%';
 
 -- List all tables in specific schema
 SELECT table_name FROM information_schema.tables WHERE table_schema='rms_p009qtzb_rms_raw';
 
-SELECT table_name FROM information_schema.tables 
-WHERE table_schema='td_idwh1_bv_prod_009_pbsdbs_ods'
-and table_name like 'tf009_vte_ligticcai';
+-- List privelege on user
+SELECT grantee, table_name, privilege_type
+FROM information_schema.role_table_grants
+WHERE grantee::text in (select  unnest(rolname||
+  ARRAY(SELECT b.rolname
+        FROM pg_catalog.pg_auth_members m
+        JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)
+        WHERE m.member = r.oid))::text
+FROM pg_catalog.pg_roles r
+WHERE r.rolname ='team_rms');
 
-GRANT USAGE ON SCHEMA rms_p009qtzb_rms_raw to team_rms;
+
+
+-- 
+do
+$$
+declare 
+	var_role_name text;
+	var_table_name text;
+	var_query_sql text;
+
+begin	
+	var_role_name = '"team_rms"';
+	
+	for var_table_name in   	
+		select
+			table_schema||'.'||table_name
+		from
+			information_schema.tables
+		where
+			table_schema like 'rms_p009qtzb_rms_%'
+	
+	loop
+		var_query_sql = 'grant select on ' ||var_table_name ||' to ' || var_role_name||';';
+	    
+		begin
+	        execute var_query_sql;
+	        exception
+	        when others then
+	            raise notice 'Error skip table, %, %',var_table_name, sqlerrm;
+	    end;
+	   
+	        raise notice '%', var_query_sql;
+	end loop;
+end;
+$$
+language 'plpgsql';
