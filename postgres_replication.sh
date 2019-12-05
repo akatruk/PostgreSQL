3aeeb45e19ed9ed60588d60b1ba81066ef4184a0
Andrey Katruk <a.katruk@live.com>
2019-12-05 16:37:54 +0300

    05.12

--- postgres_replication.sh
+++ postgres_replication.sh
@@ -30,10 +30,21 @@ or
 echo 'listen_addresses = '*'' >> /var/lib/pgsql/test/project/data/postgresql.conf 
 
 6. Run command on slave node
-pg_basebackup -h 192.168.56.101 -D /var/lib/pgsql/test2/ -R --slot=replica
+pg_basebackup -h 192.168.56.101 -D /var/lib/pgsql/test/project/data -R --slot=replica
 
 7. postgresql.conf on slave
-echo 'hot_standby = on' >> /var/lib/pgsql/test2/postgresql.auto.conf
+echo 'hot_standby = on' >> /var/lib/pgsql/test/project/data/postgresql.auto.conf
 
 ### Failover replication postgresql
 /usr/pgsql-11/bin/pg_ctl promote -D /var/lib/pgsql/test2/
+
+### Configure replica to back after failover (PG_REWIND)
+1. Create replication slot on failed server
+SELECT pg_create_physical_replication_slot('replica');
+
+2. Make password on postgres user in database
+ALTER USER postgres PASSWORD 'Vfhnvfhn123';
+3. Add on target server 
+echo "wal_log_hints = on" >> /var/lib/pgsql/test/project/data/postgresql.auto.conf
+4. Execut command
+/usr/pgsql-11/bin/pg_rewind -D /var/lib/pgsql/test/project/data/ --source-server="host=192.168.56.102 port=5432 user=postgres password=Vfhnvfhn123" -P
