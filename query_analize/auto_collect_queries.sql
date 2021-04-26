20fb573dd23db06f0563fe3ae24e40fb7c312524
akatruk <akatruk@MacBook-Pro.local>
2021-04-26 14:10:56 +0300

    k

--- query_analize/auto_collect_queries.sql
+++ query_analize/auto_collect_queries.sql
@@ -1,9 +1,10 @@
+0 * * * * psql -h localhost -U postgres < query.sql > $(date +"%Y_%m_%d_%I_%M_%p").log
+
 with CTE1 as
 (select query, calls, (total_time/calls)::integer as avg_time_ms 
 from pg_stat_statements
 where calls > 1000
 order by avg_time_ms desc)
 select * from CTE1 
-where avg_time_ms > 1000
-
-
+where avg_time_ms > 1000;
+select pg_stat_statements_reset();
